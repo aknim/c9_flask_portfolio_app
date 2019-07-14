513dda6c7b2ec53ea635292d7ae0d8fce3475432
akshaya <akshayasunny@gmail.com>
2019-07-14 22:28:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	return render_template('index.html', name=name)
+	new_name = name + "who loves coding"
+	return render_template('index.html', new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
